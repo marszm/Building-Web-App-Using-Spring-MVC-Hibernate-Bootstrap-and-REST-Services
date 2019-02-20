c8bab819d1c2093a95965b99b9f54c89eb2dd450
marszm <mariusz.szmer@gmail.com>
2019-02-20 20:25:18 +0100

    bean scopes - singelton

--- src/MyBankApp.java
+++ src/MyBankApp.java
@@ -4,9 +4,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MyBankApp {
     public static void main(String[] args){
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Account account = context.getBean("myAccount",Account.class);
-        System.out.println(account.createAccount());
-        System.out.println(account.cardDetails());
+        Account account1 = context.getBean("myAccount",Account.class);
+        Account account2 = context.getBean("myAccount",Account.class);
+        boolean isSame = (account1==account2);
+        System.out.println("acc1 = acc2: "+isSame);
+        System.out.println("acc1 hash code: "+account1.hashCode());
+        System.out.println("acc2 hash code: "+account2.hashCode());
+//        System.out.println(account.createAccount());
+//        System.out.println(account.cardDetails());
 //        System.out.println("fdsfsd");
     }
 }
